06b84b41a64a15ea7e81e708067bed5e4c51bbe7
harpreet-1 <hpsllg8@gmail.com>
2023-08-06 13:19:41 +0530

    server checking deploy

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -36,6 +36,7 @@ const registerUser = async (req, res) => {
       return "Please provide a valid email address";
     }
     const user = await UserModel.findOne({ email: email });
+    console.log(user);
     if (user) {
       return res.status(400).json({
         status: "error",
